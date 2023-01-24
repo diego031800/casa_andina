f2dd8634d59f63580d6f2360816a5cdf654d3c43
Jose Azabache Santos <aspirojose@gmail.com>
2023-01-23 21:40:30 -0500

    Initial commit

---
+++ models/dimCliente.sql
@@ -0,0 +1,46 @@
+{{ config(
+    materialized = "table"
+)}}
+
+with departamento as (
+    select 
+        *
+    from {{ source('casa_andina_dbo', 'departamento') }}
+),
+
+provincia as (
+    select 
+        *
+    from {{ source('casa_andina_dbo', 'provincia') }}
+),
+
+distrito as (
+    select 
+        *
+    from {{ source('casa_andina_dbo', 'distrito') }}
+), 
+
+cliente as (
+    select 
+        *
+    from {{ source('casa_andina_dbo', 'cliente') }}
+),
+
+dimCliente as (
+    select distinct
+        cliente.Nombre as Cliente, 
+        cliente.Sexo as Genero,
+        distrito.Descripcion as Distrito, 
+        provincia.Descripcion as Provincia, 
+        departamento.Descripcion as Departamento, 
+        cliente.IdCliente as IdCliente
+    from cliente
+    inner join distrito using (IdDistrito)
+    inner join provincia using (IdProvincia)
+    inner join departamento using (IdDepartamento)
+)
+
+select
+    ROW_NUMBER() OVER (ORDER BY dimCliente.IdCliente) AS keyCliente,
+    *
+from dimCliente
